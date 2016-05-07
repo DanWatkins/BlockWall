aa3f1fe0338a6b9491274773d776fcaa593926c9
Dan Watkins <dwatkins@valpineware.com>
2016-05-07 15:40:48 -0500

    Created the base Eto.Forms project files. Added GameBoard Eto control.

---
+++ BlockWall.Client/BlockWall.Client/Controls/GameBoard.cs
@@ -0,0 +1,30 @@
+﻿using Eto.Drawing;
+using Eto.Forms;
+
+namespace BlockWall.Client.Controls
+{
+    public class GameBoard : Scrollable
+    {
+        public GameBoard()
+        {
+            var drawable = new Drawable();
+
+            drawable.Paint += (sender, args) =>
+            {
+                var rect = new RectangleF(drawable.ClientSize);
+
+                args.Graphics.SaveTransform();
+                args.Graphics.SetClip(new RectangleF(0, 0, 200, 200));
+                args.Graphics.FillRectangle(Color.Parse("RED"), rect);
+                args.Graphics.RestoreTransform();
+            };
+
+            Content = new StackLayout
+            {
+                Spacing = 10,
+                HorizontalContentAlignment = HorizontalAlignment.Stretch,
+                Items = { new StackLayoutItem(drawable, true) }
+            };
+        }
+    }
+}
---
+++ BlockWall.Client/BlockWall.Client/MainForm.xeto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Eto.Forms;
+using Eto.Drawing;
+using Eto.Serialization.Xaml;
+
+namespace BlockWall.Client
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            XamlReader.Load(this);
+            Content = new Controls.GameBoard();
+        }
+    }
+}
